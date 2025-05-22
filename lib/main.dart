688ff5f014a719245b18bde89a032917f4a7db1f
Md Rifat Alam <mdrftlm@gmail.com>
2025-05-22 22:21:59 +0600

    first screen

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+
+import 'package:anytime_tp_app/View/first_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner:false,
+      home:FirstScreen(),
+    );
+  }
+}
+
+
+
+
+
+// void main(){
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const GetMaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home:
+//     );
+//   }
+// }
