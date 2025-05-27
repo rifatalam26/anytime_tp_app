c1dad009dc8c54b7cf8e50ea4028216c7ffcddd3
Md Rifat Alam <mdrftlm@gmail.com>
2025-05-27 21:54:55 +0600

    forget password screen

--- lib/View/Signup_screens/forget_password_screen.dart
+++ lib/View/Signup_screens/forget_password_screen.dart
@@ -12,34 +12,72 @@ class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+                image: AssetImage("assets/image/Sign up phone (2).png"))),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
             children: [
-              IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUpScreen()));
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    size: 35,
-                  )),
               const SizedBox(
-                width: 20,
+                height: 50,
               ),
-              const Text(
-                "Forget Password?",
-                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-              )
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUpScreen()));
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        size: 35,
+                      )),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Text(
+                    "Forget Password?",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 45,
+              ),
+              const Row(
+                children: [
+                  Text(
+                    "Enter Your Email",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    fillColor: const Color(0xffFDFBFB),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xffB6B6B6), width: 1),
+                        borderRadius: BorderRadius.circular(15)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xffB6B6B6), width: 1),
+                        borderRadius: BorderRadius.circular(15)),
+                    hintText: "enter your Name",
+                    hintStyle: const TextStyle(
+                        fontSize: 16, color: Color(0xff8D8D8D))),
+              ),
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
