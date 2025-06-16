d7f5cfcca1bf7dcd323fa99569ca326e2f72b4ed
Md Rifat Alam <mdrftlm@gmail.com>
2025-06-16 19:54:07 +0600

    notification screen

--- lib/View/Home_screens/notification_screen.dart
+++ lib/View/Home_screens/notification_screen.dart
@@ -75,9 +75,164 @@ class _NotificationScreenState extends State<NotificationScreen> {
                     image: const DecorationImage(
                         image: AssetImage("assets/image/profile pick2.jpeg"))),
               ),
-
+              const SizedBox(
+                width: 10,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Ralph Edwards",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Lorem ipsum dolor amet,adipiscing elit",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  Text(
+                    "dolor sit amet,adipiscing elit.  ",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              const Text(
+                "2h",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Color(0xff1C1F5E)),
+              )
             ],
-          )
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 15,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.black, width: 2),
+                    image: const DecorationImage(
+                      fit: BoxFit.cover,
+                        image: AssetImage("assets/image/profile pic3.jpeg"))),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Ralph Edwards",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Lorem ipsum dolor amet,adipiscing elit",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  Text(
+                    "dolor sit amet,adipiscing elit.  ",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              const Text(
+                "3h",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Color(0xff1C1F5E)),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 15,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.black, width: 2),
+                    image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/image/profile pic4.jpeg"))),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Ralph Edwards",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Lorem ipsum dolor amet,adipiscing elit",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  Text(
+                    "dolor sit amet,adipiscing elit.  ",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              const Text(
+                "4h",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Color(0xff1C1F5E)),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 15,
+              ),
+              Text(
+                "Yesterday",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ],
       ),
     );
