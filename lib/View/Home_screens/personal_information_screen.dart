bbc3fa1e8e0049f72c5e346990f7805ca0bf8221
Md Rifat Alam <mdrftlm@gmail.com>
2025-06-18 21:42:44 +0600

    personal information screen

--- lib/View/Home_screens/personal_information_screen.dart
+++ lib/View/Home_screens/personal_information_screen.dart
@@ -33,10 +33,91 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
                   border: Border.all(color: Colors.white, width: 2),
                   image: const DecorationImage(
                       fit: BoxFit.cover,
-                      image:
-                      AssetImage("assets/image/profile picture.webp"))),
+                      image: AssetImage("assets/image/profile picture.webp"))),
             ),
-            Text("data")
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Henry Johnson",
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Column(
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.person_outline,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "Milan Jack",
+                      style: TextStyle(fontSize: 17),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "+44 26537 26347",
+                      style: TextStyle(fontSize: 17),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.mail_outline,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "example@gmail.com",
+                      style: TextStyle(fontSize: 17),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "24 NY, Florida, Miami ",
+                      style: TextStyle(fontSize: 17),
+                    )
+                  ],
+                ),
+              ],
+            )
           ],
         ),
       ),
