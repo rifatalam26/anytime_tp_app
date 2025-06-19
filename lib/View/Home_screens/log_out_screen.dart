70c00394ad74119241218eb97e8adc26efd802c3
Md Rifat Alam <mdrftlm@gmail.com>
2025-06-19 22:03:44 +0600

    personal information screen ok

---
+++ lib/View/Home_screens/log_out_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogOutScreen extends StatefulWidget {
+  const LogOutScreen({super.key});
+
+  @override
+  State<LogOutScreen> createState() => _LogOutScreenState();
+}
+
+class _LogOutScreenState extends State<LogOutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
